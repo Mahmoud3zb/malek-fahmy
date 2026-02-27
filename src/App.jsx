43a3a25a6940cc421ab8269cec240aa566878367
Mahmoud Azab <mahmoudazaab034@gmail.com>
2026-02-27 08:57:10 +0200

    new fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./component/header/header";
-import Section from "./component/section/section";
+import Header from "./component/header/Header";
+import Section from "./component/section/Section";
 import Footer from "./component/Footer/Footer";
 
 function App() {
